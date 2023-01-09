48c3788a5bc5807bb13e4e8bd2043414198508e9
valentinlamine <valentinlamine@outlook.fr>
2023-01-09 10:35:15 +0100

    Correction des liens

--- main.go
+++ main.go
@@ -21,13 +21,13 @@ func main() {
 	script := http.FileServer(http.Dir("script"))
 	http.Handle("/script/", http.StripPrefix("/script/", script))
 	//gestion html
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/Achat", buyHandler)
-	http.HandleFunc("/QuiSommesNous", whoareusHandler)
-	http.HandleFunc("/MonCompte", accountHandler)
-	http.HandleFunc("/Inscription", registerHandler)
-	http.HandleFunc("/Resultats", resultHandler)
-	http.HandleFunc("/Erreur", errorHandler)
+	http.HandleFunc("/", IndexHandler)
+	http.HandleFunc("/Achat", AchatHandler)
+	http.HandleFunc("/QuiSommesNous", QuiSommesNousHandler)
+	http.HandleFunc("/MonCompte", MonCompteHandler)
+	http.HandleFunc("/Inscription", InscriptionHandler)
+	http.HandleFunc("/Resultats", ResultatHandler)
+	http.HandleFunc("/Erreur", ErreurHandler)
 	http.ListenAndServe(":80", nil)
 }
 func alreadyregistered(email string) bool {
@@ -87,7 +87,7 @@ func register(email string, password string) {
 	}
 }
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl1 := template.Must(template.ParseFiles("templates/index.html"))
 	if r.Method != http.MethodPost {
 		tmpl1.Execute(w, nil)
@@ -96,22 +96,22 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl1.Execute(w, nil)
 }
 
-func buyHandler(w http.ResponseWriter, r *http.Request) {
+func AchatHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl2 := template.Must(template.ParseFiles("templates/Achat.html"))
 	tmpl2.Execute(w, nil)
 }
 
-func whoareusHandler(w http.ResponseWriter, r *http.Request) {
+func QuiSommesNousHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl3 := template.Must(template.ParseFiles("templates/quisommesnous.html"))
 	tmpl3.Execute(w, nil)
 }
 
-func accountHandler(w http.ResponseWriter, r *http.Request) {
+func MonCompteHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl4 := template.Must(template.ParseFiles("templates/MonCompte.html"))
 	tmpl4.Execute(w, nil)
 }
 
-func registerHandler(w http.ResponseWriter, r *http.Request) {
+func InscriptionHandler(w http.ResponseWriter, r *http.Request) {
 	if r.FormValue("email") != "" {
 		if alreadyregistered(r.FormValue("email")) {
 			http.Redirect(w, r, "/Erreur", http.StatusSeeOther)
@@ -122,13 +122,13 @@ func registerHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl5.Execute(w, nil)
 }
 
-func resultHandler(w http.ResponseWriter, r *http.Request) {
+func ResultatHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.FormValue("search"))
 	tmpl6 := template.Must(template.ParseFiles("templates/Resultats.html"))
 	tmpl6.Execute(w, nil)
 }
 
-func errorHandler(w http.ResponseWriter, r *http.Request) {
+func ErreurHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl7 := template.Must(template.ParseFiles("templates/Erreur.html"))
 	tmpl7.Execute(w, nil)
 }
