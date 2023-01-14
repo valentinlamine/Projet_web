2a09e3f9278ed977787d4b02eba91de45cb5e983
valentinlamine <87501303+valentinlamine@users.noreply.github.com>
2023-01-14 16:36:54 +0100

    début rensponsive

--- script/car.js
+++ script/car.js
@@ -1,16 +1,11 @@
 function openPopUp(element) {
     element = element.parentElement.parentElement;
     element.nextElementSibling.style.display = "flex";
-    document.body.style.overflow = "hidden";
-    //element.style.display = "none";
-    //element.nextElementSibling.scrollIntoView({behavior: "smooth", block: "center", inline: "center"});
-
+    //document.body.style.overflow = "hidden";
 }
+
 function closePopUp(element) {
     element = element.parentElement.parentElement.parentElement;
     document.body.style.overflow = "auto";
     element.style.display = "none";
-    //element.previousElementSibling.style.display = "flex";
-    //element.previousElementSibling.scrollIntoView({behavior: "smooth", block: "center", inline: "center"});
-
 }
